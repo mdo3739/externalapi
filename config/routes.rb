ee000450007faffeb244ef971d36e26f4f8f36b4
Michael Do <mdo3739@gmail.com>
2015-08-12 22:12:44 -0500

    added create methods for all models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root to: 'welcome#index'
   namespace :api, defaults: {format: :json} do
-    resources :users
+    resources :users do
+      resources :lists
+    end
+
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
   end
+
 end
